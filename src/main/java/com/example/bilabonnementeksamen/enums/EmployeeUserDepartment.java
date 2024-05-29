eb5092ac28bc0e63342d09393fb9dc416bf3a0a8
VictorRasmussenKEA <vira0003@stud.kea.dk>
2024-05-29 07:52:57 +0200

    added jar

--- src/main/java/com/example/bilabonnementeksamen/enums/EmployeeUserDepartment.java
+++ src/main/java/com/example/bilabonnementeksamen/enums/EmployeeUserDepartment.java
@@ -3,5 +3,5 @@ package com.example.bilabonnementeksamen.enums;
 public enum EmployeeUserDepartment {
     DATA_REGISTRATION,
     DAMAGE_AND_REPAIR,
-    BUSINESS_DEVELOPER
+    BUSINESS_DEVELOPERS
 }
